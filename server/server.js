a6e931241424e6f0b42def64a6a56de5b1220018
MadTofu22 <stutler.tom@gmail.com>
2020-10-12 01:30:42 -0500

    fixed heroku dynamic port issue

--- server/server.js
+++ server/server.js
@@ -3,7 +3,7 @@ const bodyParser = require('body-parser');
 const app = express();
 const port = 5000;
 
-app.listen(port, () => {
+app.listen(process.env.PORT || port, () => {
 console.log('Up and running on port', port);
 });
 
